e7e829b7076789ffe9bd313c2f03202757996e3c
Adna <adna.maksumic@edu.fit.ba>
2025-05-06 15:00:18 +0200

    Implement Match class

--- src/main/java/org/scoreboard/Main.java
+++ src/main/java/org/scoreboard/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.scoreboard;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/org/scoreboard/Match.java
@@ -0,0 +1,38 @@
+package org.scoreboard;
+
+public class Match {
+
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private final long startTime;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.startTime = System.nanoTime();
+    }
+
+    public String getHomeTeam() { return homeTeam; }
+    public String getAwayTeam() { return awayTeam; }
+    public int getHomeScore() { return homeScore; }
+    public int getAwayScore() { return awayScore; }
+    public long getStartTime() { return startTime; }
+
+    public void updateScore(int homeScore, int awayScore) {
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public int getTotalScore() {
+        return homeScore + awayScore;
+    }
+
+    @Override
+    public String toString() {
+        return homeTeam + " " + homeScore + " - " + awayTeam + " " + awayScore;
+    }
+}
